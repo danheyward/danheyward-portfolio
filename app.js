838c322f94ae71d308a58d170b496e82561a38b1
Daniel Heyward <danheyward@gmail.com>
2018-04-08 13:06:08 -0700

    added skills yo

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
 
+  $("#who-link").click(function() {
+      $('html,body').animate({
+          scrollTop: $("#who").offset().top},
+          'slow');
+  });
 
+  $("#what-link").click(function() {
+      $('html,body').animate({
+          scrollTop: $("#what").offset().top},
+          'slow');
+  });
 
+  $("#how-link").click(function() {
+      $('html,body').animate({
+          scrollTop: $("#how").offset().top},
+          'slow');
+  });
 
 
 
